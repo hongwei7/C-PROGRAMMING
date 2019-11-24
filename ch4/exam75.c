6e70611db0083e8e8ab0d2e0f154620e97f98671
hongwei7 <1216869567@qq.com>
2019-11-24 23:06:45 +0800

    Update exam75.c

--- ch4/exam75.c
+++ ch4/exam75.c
@@ -13,11 +13,11 @@ int main()
 	double op2;
 	char s[MAXOP];
 
-	while((type==getop(s)) !=EOF){
+	while((type==getop(s)) != EOF){
 		switch(type){
 			case  NUMBER:push(atof(s)); break;
 			case '+':push(pop()+pop());break;
-			case '*':push((pop()*pop());break;
+			case '*':push((pop()*pop()));break;
 			case '-':op2=pop(); push(pop()-op2);break;
 			case '/':op2=pop();
 				if(op2!=0.0)
