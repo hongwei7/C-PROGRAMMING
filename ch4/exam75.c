718fd3dad90cbc8ea64ce609119bb7e126eaa5b9
hongwei7 <1216869567@qq.com>
2019-11-24 17:05:24 +0100

    add

--- ch4/exam75.c
+++ ch4/exam75.c
@@ -1,12 +1,67 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-#define MAXOP 100
 #define NUMBER '0'
 
-int getop(char []);
-void push(double);
-double pop(void);
+//*****************getop****************
+#include<ctype.h>
+int getch(void);
+void ungetch(int);
+
+int getop(char s[])
+{
+	int i,c;
+	while((s[0]=c=getch())==' '||c=='\t');
+	s[1]='0';
+	if(isdigit(c)&&c!='.')
+		return c;
+	i=0;
+	if(isdigit(c))
+		while(isdigit(s[++i]=c=getch()));
+	if(c=='.')
+		while(isdigit(s[++i]=c=getch()));
+	s[i]='0';
+	if(c!=EOF)
+		ungetch(c);
+	return NUMBER;
+}
+#define BUFSIZE 100
+char buf[BUFSIZE];
+int bufp=0;
+int getch(void)
+{
+	return (bufp>0)?buf[--bufp]:getchar();
+}
+void ungetch(int c)
+{
+	if(bufp>=BUFSIZE)
+		printf("ungetch: too many characters\n");
+	else
+		buf[bufp++]=c;
+}
+
+//***************stack*******************
+#define MAXVAL 100
+int sp=0;
+double val[MAXVAL];
+void push(double f)
+{
+	if(sp<MAXVAL)
+		val[sp++]=f;
+	else
+		printf("error:stack out of range");
+}
+double pop(void)
+{
+	if(sp<0)
+		printf("error:stack empty!");
+	else
+		return val[--sp];
+	return 0.0;
+}
+
+//****************main******************
+#define MAXOP 100
+
 int main()
 {
 	int type;
@@ -14,6 +69,7 @@ int main()
 	char s[MAXOP];
 
 	while((type==getop(s)) != EOF){
+		printf("--- %c ---\n");
 		switch(type){
 			case  NUMBER:push(atof(s)); break;
 			case '+':push(pop()+pop());break;
