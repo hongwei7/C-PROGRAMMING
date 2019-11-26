4c099e27237b94447bdf052e14b4d7fdd5e6ea66
hongwei7 <1216869567@qq.com>
2019-11-27 00:19:36 +0800

    add

---
+++ ch5/E5-1.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define BUFSIZE 100
+ char buf[BUFSIZE];
+ int bufp=0;
+ int getch(void)
+ {
+     return (bufp>0)?buf[--bufp]:getchar();
+ }
+ void ungetch(int c)
+ {
+       if(bufp>=BUFSIZE)
+         printf("ungetch: too many characters\n");
+     else
+         buf[bufp++]=c;
+ }
+void ungets(char s[])
+ {
+     int i=0;
+     for(;s[i]!='\0';i++);
+     for(;i>=0;i--)
+     {
+         ungetch(s[i]);
+     }
+ }
+int getint(int *pn)
+{
+    int c,sign;
+    while(isspace(c=getch()))
+        ;
+    if(!isdigit(c)&&c!=EOF&&c!='-'){
+        ungetch(c);
+        return 0;
+    }
+    sign=(c=='-')?-1:1;
+    if(c=='+'||c=='-')
+        c=getch();
+    for(*pn=0;isdigit(c);c=getch())
+        *pn=10*(*pn)+c-'0';
+    *pn*=sign;
+    if(c!=EOF)
+        ungetch(c);
+    return c;
+}
+int main()
+{
+    int a=0;
+    printf("a\t%d\n",a);
+    getint(&a);
+    printf("a\t%d\n",a);
+    return 0;
+}
+
